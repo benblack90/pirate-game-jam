cceb8f8097220c156630911260d3437327698e0b
Ben Schwarz <100788839+benblack90@users.noreply.github.com>
2024-01-17 15:20:39 +0000

    Added basic spine of game logic
    Added GameController, Level, and Player classes, intended to form the back-bone of the game's logic.

---
+++ Pirate Game/Assets/Ben/Level.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    public PracticeComputeScript gooController;
+    public Player player;
+    public Camera mainCam;
+
+    List<GameObject> staticDestructables = new List<GameObject>();
+    List<GameObject> dynamicDestructables = new List<GameObject>();
+    Vector3 playerStart;
+
+    public void InitLevel()
+    {
+        ReadLevelFromFile();
+        InitPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCamera(player.playerModel.transform.position);
+        UpdateStatics();
+        UpdateDynamics();
+    }
+
+    void InitPlayer()
+    {
+        player.SetStartPos(playerStart);
+    }
+
+    void UpdateStatics()
+    {
+        foreach(GameObject o in staticDestructables)
+        {
+            //check if adjacent to goo'd tile
+                //check its temperature
+                    //ignite if appropriate
+                    //reduce hp if necessary
+                //check if on fire
+                    //reduce hp if necessary
+            //if destroyed ... 
+                //replace model with destroyed version
+                //change tiles directly beneath to BLANK
+                //change adjacent goo tiles to GOO_SPREADABLE
+                //give points to player
+                //remove from staticDestructables list
+        }
+    }
+
+    void UpdateDynamics()
+    {
+        foreach (GameObject o in dynamicDestructables)
+        {
+            //check if adjacent to goo'd tile
+                //check temperature
+                //replace with burning/burnt model
+                //give points to player
+                //remove from dynamicDestructables list
+        }
+    }
+
+    void UpdateCamera(Vector3 playerPos)
+    {
+        mainCam.transform.position = playerPos + new Vector3(0, 20, 0);
+    }
+
+    void ReadLevelFromFile()
+    {
+        //TODO - make this do the level reading please, Alex
+
+        //this is temporary, just to make the game work in the absence of the actual level editor
+        playerStart = new Vector3(0,0,0);
+    }
+}
